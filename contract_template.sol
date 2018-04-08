01cb7c59abd348726db656cf791b6e5d3fee8071
Limor Ganon <limor@Limors-MacBook-Pro.local>
2018-04-08 13:06:41 +0300

    First commit of the project

---
+++ contract_template.sol
@@ -0,0 +1,85 @@
+pragma solidity ^0.4.11;
+
+
+// ----------------------------------------------------------------------------
+// ERC Token Standard #20 Interface
+// https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+// ----------------------------------------------------------------------------
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract <MY_TOKEN_CONTRACT_NAME> is ERC20 {
+
+
+    mapping (address => uint256) balances;
+    mapping (address => mapping (address => uint256)) allowed;
+    uint256 public totalSupply;
+    
+    string public name;                   //fancy name: eg Simon Bucks
+    uint8 public decimals;                //How many decimals to show. ie. There could 1000 base units with 3 decimals. Meaning 0.980 SBX = 980 base units. It's like comparing 1 wei to 1 ether.
+    string public symbol;                 //An identifier: eg SBX
+
+
+    function <MY_TOKEN_CONTRACT_NAME>() {
+    
+        totalSupply = <MY_TOTAL_SUPPLY>;                        // Update total supply (100000 for example)
+        balances[msg.sender] = totalSupply;               // Give the creator all initial tokens (100000 for example)
+        name = "<MY_TOKEN_NAME>";                               // Set the name for display purposes
+        decimals = <MY_DECIMAL_PLACES>;                         // Amount of decimals for display purposes
+        symbol = "<MY_TOKEN_SYMBOL>";                           // Set the symbol for display purposes
+
+    }
+    
+    function totalSupply() public constant returns (uint) {
+        return totalSupply;
+    }
+    
+    function transfer(address _to, uint256 _value) returns (bool success) {
+        
+        //Default assumes totalSupply can't be over max (2^256 - 1).
+        //If your token leaves out totalSupply and can issue more tokens as time goes on, you need to check if it doesn't wrap.
+        //Replace the if with this one instead.
+        //if (balances[msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[msg.sender] >= _value && _value > 0) {
+            balances[msg.sender] -= _value;
+            balances[_to] += _value;
+            Transfer(msg.sender, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
+        //same as above. Replace this line with the following if you want to protect against wrapping uints.
+        //if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && balances[_to] + _value > balances[_to]) {
+        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
+            balances[_to] += _value;
+            balances[_from] -= _value;
+            allowed[_from][msg.sender] -= _value;
+            Transfer(_from, _to, _value);
+            return true;
+        } else { return false; }
+    }
+
+    function balanceOf(address _owner) constant returns (uint256 balance) {
+        return balances[_owner];
+    }
+
+    function approve(address _spender, uint256 _value) returns (bool success) {
+        allowed[msg.sender][_spender] = _value;
+        Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
+    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
+      return allowed[_owner][_spender];
+    }
+}
---
+++ server.js
@@ -0,0 +1,183 @@
+//
+// # SimpleServer
+//
+// A simple chat server using Socket.IO, Express, and Async.
+//
+var http = require('http');
+var path = require('path');
+
+var async = require('async');
+var socketio = require('socket.io');
+var express = require('express');
+
+//
+// ## SimpleServer `SimpleServer(obj)`
+//
+// Creates a new instance of SimpleServer with the following options:
+//  * `port` - The HTTP port to listen on. If `process.env.PORT` is set, _it overrides this value_.
+//
+var router = express();
+var server = http.createServer(router);
+var io = socketio.listen(server);
+
+router.use(express.static(path.resolve(__dirname, 'client')));
+var coins = [];
+var messages = [];
+var sockets = [];
+
+io.on('connection', function (socket) {
+    coins.forEach(function (data) {
+      socket.emit('newCoin', data);
+    });
+
+    sockets.push(socket);
+
+    socket.on('disconnect', function () {
+      sockets.splice(sockets.indexOf(socket), 1);
+      updateRoster();
+    });
+
+    
+    socket.on('message', function (msg) {
+      var text = String(msg || '');
+
+      if (!text)
+        return;
+
+      console.log("socket.on. text is:", text );
+      
+      socket.get('name', function (err, name) {
+        var data = {
+          name: name,
+          text: text
+        };
+
+        console.log("socket.on. name is:", name );
+        
+        broadcast('message', data);
+        messages.push(data);
+      });
+    });
+
+    socket.on('identify', function (name) {
+      socket.set('name', String(name || 'Anonymous'), function (err) {
+        updateRoster();
+      });
+    });
+    
+    socket.on('newCoin', function(coin) {
+         console.log("newCoin event, coin data: ", coin);
+         
+         var templateContract = readContractTemplate();
+         var newContractCode = customizeContract(templateContract, coin.name, coin.symbol, coin.supply, coin.decimal);
+         
+         coin.contract = newContractCode;
+         coins.push(coin);
+         
+         socket.emit('newCoin', coin);
+         //broadcast('message', coinName);
+    });
+  });
+
+function updateRoster() {
+  async.map(
+    sockets,
+    function (socket, callback) {
+      socket.get('name', callback);
+    },
+    function (err, names) {
+      console.log("updateRoster names:", names );
+      broadcast('roster', names);
+    }
+  );
+}
+
+function broadcast(event, data) {
+  sockets.forEach(function (socket) {
+    console.log("Broadcast data:", data );
+    socket.emit(event, data);
+  });
+}
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
+  var addr = server.address();
+  console.log("Chat server listening at", addr.address + ":" + addr.port);
+});
+
+
+function customizeContract(templateContract, tokenName, tokenSymbol, tokenTotalSupply, tokenDecimalPlaces) {
+  
+  console.log("customizeContract");
+  
+  compileContractTest();
+  
+  var contractName = removeWhiteSpace(tokenName);
+  
+  //console.log("contractName is:" , contractName);
+
+  var newContract = templateContract;
+  
+  newContract = newContract.replace("<MY_TOKEN_NAME>", tokenName);
+  newContract = newContract.replace("<MY_TOKEN_SYMBOL>", tokenSymbol);
+  newContract = newContract.replace("<MY_TOTAL_SUPPLY>", tokenTotalSupply);
+  newContract = newContract.replace("<MY_DECIMAL_PLACES>", tokenDecimalPlaces);
+  newContract = newContract.replace("<MY_TOKEN_CONTRACT_NAME>", contractName);
+  newContract = newContract.replace("<MY_TOKEN_CONTRACT_NAME>", contractName);
+  
+  //console.log("newContract is:" , newContract);
+  
+  return newContract;
+}
+
+// gets a string and returns a string without any whitespaces
+function removeWhiteSpace(str) {
+  return str.replace(/\s/g, "");
+}
+
+function readContractTemplate() {
+  
+  var fs = require('fs');
+  return templateContract = fs.readFileSync('contract_template.sol', 'utf8');
+  
+}
+
+
+
+function compileContractTest() {
+
+  console.log("LIMOR1");
+  var solc = require('solc');
+  console.log("LIMOR2");
+
+  var input = "contract x { function g() {} }";
+  var output = solc.compile(input, 1); // 1 activates the optimiser
+  
+    console.log("LIMOR3, output: ", output);
+
+  for (var contractName in output.contracts) {
+      // code and ABI that are needed by web3
+      console.log("LIMOR4", contractName + ': ' + output.contracts[contractName].bytecode);
+      console.log("LIMOR5", contractName + '; ' + JSON.parse( output.contracts[contractName].interface));
+  }
+
+}
+
+function compileContract(contractCode) {
+  console.log("111");
+  
+  const fs = require("fs");
+  console.log("222");
+  const solc = require('solc');
+  console.log("333");
+  let Web3 = require('web3');
+  console.log("444");
+  
+  let web3 = new Web3();
+  web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
+  
+  
+  let compiledContract = solc.compile(contractCode, 1);
+  
+  console.log("222");
+}
+  
